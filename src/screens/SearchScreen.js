050bb6cf01842e2032b81a2ab9e668c26500b030
Onifade Demola <devep.aonifade@gmail.com>
2019-11-30 09:18:48 +0100

    first commit local

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import SearchBar from '../components/SearchBar'
+
+const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    
+    return <View>
+        <SearchBar 
+            term={term} 
+            onTermChange={newTerm => setTerm(newTerm)}
+            onTermSubmit={() => console.log('term has been submitted')}
+        />
+        <Text>Search the screen</Text>
+        <Text>{term}</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
